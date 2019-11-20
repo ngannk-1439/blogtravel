e773b912ba055cc5f25e4fb98aa2163256bd2567
hungbnx-1830 <nguyen.xuan.hungb@sun-asterisk.com>
2019-11-20 13:52:49 +0700

    Task sidebar

--- app/containers/Sider/index.js
+++ app/containers/Sider/index.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
 import React from 'react';
 import 'antd/dist/antd.css';
+import styled from 'styled-components';
+import { colors } from '../../global-styles';
 
+import SubjectList from 'containers/SubjectList';
+
+
+
+const Title = styled.h2`
+  background: ${colors.primaryColor} !important;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: none;
+  margin-bottom: 0;
+  height: 50px;
+  text-transform: uppercase;
+`;
 
 function MainSider() {
   return (
-    <div>This is right sider</div>
+    <React.Fragment>
+      <Title>Chủ Đề</Title>
+      <SubjectList></SubjectList>
+    </React.Fragment>
+
   )
 }
 
